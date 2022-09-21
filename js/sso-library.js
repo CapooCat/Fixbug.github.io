466bff92507c568541e3580b8b9f0d012989fea6
CapooCat <54877892+CapooCat@users.noreply.github.com>
2022-09-21 10:55:26 +0700

    Update sso-library.js

--- js/sso-library.js
+++ js/sso-library.js
@@ -122,17 +122,18 @@ $("body").on("input", ".des-input", function (e) {
             $(this).val(insert(origin_val, origin_index, e.originalEvent.data.slice(-1)))
             start = origin_index;
             end = origin_index;
-            origin_index = null;
         } else {
             $(this).val(insert(origin_val, start - 1, e.originalEvent.data.slice(-1)));
         }
+
+        origin_index = null;
         origin_val = null;
     }
     
     var value = $(this).val().toString().replaceAll(" ", "");
     if(e.originalEvent.data === " ") {
         origin_val = value;
-        origin_index = start - 1;
+        origin_index = start;
     }
 
     $(this).val(value);
