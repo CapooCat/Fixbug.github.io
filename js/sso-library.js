b45889657cb70b91858870dfb0d4e5b8c99b495a
CapooCat <54877892+CapooCat@users.noreply.github.com>
2022-11-18 15:24:42 +0700

    s

--- js/sso-library.js
+++ js/sso-library.js
@@ -18,8 +18,8 @@ $(document).ready(function() {
     }
 
     if(isIOSDevice()) {
-        $('input[type=text].des-input').removeClass("password");
-        $('input[type=text].des-input').prop("type", "password");
+        $('input[type=text].des-input.password').removeClass("password");
+        $('input[type=text].des-input.password').prop("type", "password");
     }
 });
 
